ee71bf38fd6a448f629b83c18c72908f2436a9ed
Kurt Nelson <kurtisnelson@gmail.com>
2014-10-31 21:47:14 -0700

    Fix to proper material appcompat

--- mobile/src/main/java/com/dappervision/android/wearscript/controller/MobileMainActivity.java
+++ mobile/src/main/java/com/dappervision/android/wearscript/controller/MobileMainActivity.java
@@ -1,25 +1,65 @@
 package com.dappervision.android.wearscript.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 import com.crittercism.app.Crittercism;
 import com.crittercism.app.CrittercismConfig;
 import com.dappervision.android.wearscript.BuildConfig;
 import com.dappervision.android.wearscript.MobileWearScriptInfo;
-import com.dappervision.wearscript.launcher.MainActivity;
+import com.dappervision.wearscript.Utils;
 import com.dappervision.wearscript.launcher.WearScriptInfo;
 
-public class MobileMainActivity extends MainActivity {
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
-    @Override
-    protected Fragment createFragment() {
-        return ScriptListFragment.newInstance();
-    }
+public class MobileMainActivity extends ActionBarActivity implements ScriptListFragment.Callbacks {
+    private static final String TAG = "MainActivity";
+    private static final boolean DBG = false;
+    private static final String GISTS_PATH = "gists/";
+    private static final String WEARSCRIPT_PATH = Utils.dataPath() + GISTS_PATH;
+    private boolean launchScriptList = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutResId());
+        String gist = Utils.getPackageGist(this);
+        if (DBG) Log.d(TAG, "Gist id is " + gist);
+        if (gist != null) {
+            if (DBG) Log.d(TAG, "Found package name gist id " + gist);
+            byte[] manifestData = Utils.LoadData(GISTS_PATH + gist, "manifest.json");
+            if (manifestData != null) {
+                JSONObject manifest = (JSONObject) JSONValue.parse(new String(manifestData));
+                if (manifest != null && manifest.containsKey("name")) {
+                    String filePath = WEARSCRIPT_PATH + gist + "/" + "glass.html";
+                    WearScriptInfo wsInfo = buildInfoForGist(manifest, filePath);
+                    Intent intent = wsInfo.getIntent();
+                    int flags = intent.getFlags() | Intent.FLAG_ACTIVITY_SINGLE_TOP;
+                    intent.setFlags(flags);
+                    startActivity(intent);
+                    launchScriptList = false;
+                } else {
+                    if (DBG) Log.d(TAG, "Didn't find gist name in manifest.");
+                }
+            } else {
+                // Load from apk assets
+                if (DBG) Log.d(TAG, "Loading script from apk assets");
+                String filePath = "/android_asset/" + gist + "/" + "glass.html";
+                WearScriptInfo wsInfo = buildInfoForApkAsset(null);
+                Intent intent = wsInfo.getIntent();
+                int flags = intent.getFlags() | Intent.FLAG_ACTIVITY_SINGLE_TOP;
+                intent.setFlags(flags);
+                startActivity(intent);
+                launchScriptList = false;
+            }
+        }
+
         if(!BuildConfig.DEBUG) {
             CrittercismConfig config = new CrittercismConfig();
             config.setLogcatReportingEnabled(true);
@@ -27,12 +67,40 @@ public class MobileMainActivity extends MainActivity {
         }
     }
 
-    protected WearScriptInfo buildInfoForPath(String name, String filePath) {
-        return new MobileWearScriptInfo(this, name, filePath);
+    protected WearScriptInfo buildInfoForApkAsset(String filePath) {
+        return buildInfoForPath("APK Script", filePath);
+    }
+
+    protected WearScriptInfo buildInfoForGist(JSONObject manifest, String filePath) {
+        return buildInfoForPath((String) manifest.get("name"), filePath);
+    }
+
+    protected int getLayoutResId() {
+        return com.dappervision.wearscript.R.layout.activity_fragment;
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        if (DBG) Log.d(TAG, "LifeCycle: onResume");
+        if (launchScriptList) {
+            FragmentManager manager = getSupportFragmentManager();
+            Fragment fragment = createFragment();
+            manager.beginTransaction()
+                    .replace(com.dappervision.wearscript.R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
     public void onScriptSelected(WearScriptInfo scriptInfo) {
         startActivity(scriptInfo.getIntent());
+    }
+
+    protected Fragment createFragment() {
+        return ScriptListFragment.newInstance();
+    }
+
+    protected WearScriptInfo buildInfoForPath(String name, String filePath) {
+        return new MobileWearScriptInfo(this, name, filePath);
     }
 }
